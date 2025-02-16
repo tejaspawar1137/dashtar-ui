bf2f0781bc3df82fbf5b274c45fd67840282c128
Jigyasa <jigyasa@jirehsol.com>
2025-02-16 17:49:12 +0530

    remokgd

--- src/components/sidebar/DesktopSidebar.jsx
+++ src/components/sidebar/DesktopSidebar.jsx
@@ -3,7 +3,7 @@ import SidebarContent from "@/components/sidebar/SidebarContent";
 
 const DesktopSidebar = () => {
   return (
-    <aside className="z-30 flex-shrink-0 hidden shadow-sm w-64 overflow-y-auto bg-white dark:bg-gray-800 lg:block">
+    <aside className="z-30 flex-shrink-0 hidden shadow-sm w-64 overflow-y-auto bg-white  lg:block">
       <SidebarContent />
     </aside>
   );
